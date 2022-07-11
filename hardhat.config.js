fcb4f1ea263d28f2ff02cc40ff48d0cbf4aea2b5
karangorania <karangoraniya@gmail.com>
2022-07-11 18:39:59 +0530

    aurora network  added 🚀

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,42 @@
-require("@nomicfoundation/hardhat-toolbox");
+require('@nomiclabs/hardhat-waffle');
+require('hardhat-gas-reporter');
+require('@nomiclabs/hardhat-etherscan');
 
-/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+
+const { PRIVATE_KEY, POLYGON_SCAN_KEY } = process.env;
+
 module.exports = {
-  solidity: "0.8.9",
+  gasReporter: {
+    currency: 'ETH',
+    gasPrice: 21,
+  },
+  solidity: {
+    version: '0.8.9',
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      gas: 12000000,
+      gasPrice: 8000000000,
+      blockGasLimit: 0x1fffffffffffff,
+      allowUnlimitedContractSize: true,
+      timeout: 1800000,
+    },
+    testnet_aurora: {
+      url: 'https://testnet.aurora.dev',
+      accounts: {
+        mnemonic: PRIVATE_KEY,
+      },
+      chainId: 1313161555,
+    },
+  },
+  etherscan: {
+    apiKey: POLYGON_SCAN_KEY,
+  },
 };
